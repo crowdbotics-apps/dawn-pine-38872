dc3e90a6759b1854ac6ecd284f57c860b67e0826
Crowdbotics <team@crowdbotics.com>
2023-02-13 07:34:28 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "dawn-pine-38872",
+  url: "https://dawn-pine-38872.botics.co",
+  api: "https://dawn-pine-38872.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
